97df962fa9d5e8d4578e3b798596d5026ae02df7
Matthew <subinmatt@gmail.com>
2023-01-28 14:55:24 -0800

    first commit

---
+++ python-containers.py
@@ -0,0 +1,8 @@
+# 1. Create a list named studentscontaining some student names (strings).
+# Print out the second student's name.
+# Print out the last student's name.
+
+student = ['Matt', 'Ji', 'Cody', 'Colin', 'Krissy']
+
+print(student[1])
+print(student[4])
